2c0dacbee7317d1f887d399018302e3dae18f905
mannyaalonso <mannyaalonso@gmail.com>
2023-03-03 10:36:11 -0500

    registration/signin icon

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,4 +1,5 @@
 import { LockClosedIcon } from "@heroicons/react/20/solid"
+import { AiOutlineStock } from "react-icons/ai"
 import { useNavigate } from "react-router-dom"
 import { useState } from 'react'
 
@@ -33,10 +34,8 @@ export default function Registration () {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
+            <AiOutlineStock
+              className="mx-auto h-12 w-auto text-indigo-600"
             />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Register for Stockwise
