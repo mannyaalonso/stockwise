8ea4afa9364f0da33ab4d76873e6f85999f5cd7c
Jonshea Nutson <yuurieru@icloud.com>
2023-03-06 12:32:09 -0500

    changed route in post services

--- src/services/PostServices.js
+++ src/services/PostServices.js
@@ -1,50 +1,51 @@
-import Client from "./api"
+import Client from './api'
 
 export const CheckWatchlist = async () => {
-  try {
-    const res = await Client.get(`/api/watchlists/${localStorage.getItem('userId')}`)
-    if (res.data == "") {
-      const res = await Client.post(
-        `/api/watchlists/${localStorage.getItem("userId")}`
+    try {
+        const res = await Client.get(
+            `/api/watchlists/${localStorage.getItem('userId')}`
         )
-        localStorage.setItem("watchlistId", res.data.id)
-    } else {
-      localStorage.setItem('watchlistId', res.data.id)
+        if (res.data == '') {
+            const res = await Client.post(
+                `/api/watchlists/${localStorage.getItem('userId')}`
+            )
+            localStorage.setItem('watchlistId', res.data.id)
+        } else {
+            localStorage.setItem('watchlistId', res.data.id)
+        }
+    } catch (error) {
+        console.log(error)
     }
-  } catch (error) {
-    console.log(error)
-  }
 }
 
 export const PostStock = async (stock) => {
-  try {
-    const res = await Client.post(
-      `/api/stocks/${localStorage.getItem("watchlistId")}`,
-      { name: stock }
-    )
-    return res.data
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+        const res = await Client.post(
+            `/api/stocks/${localStorage.getItem('watchlistId')}/${stock.name}}`,
+            { name: stock }
+        )
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export const GetAllStocks = async () => {
-  try {
-    const res = await Client.get(
-      `/api/stocks/${localStorage.getItem("watchlistId")}`)
-    return res.data
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+        const res = await Client.get(
+            `/api/stocks/${localStorage.getItem('watchlistId')}`
+        )
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export const DestroyStock = async (id) => {
-  try {
-    const res = await Client.delete(
-      `/api/stocks/${id}`
-    )
-    return res.data
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+        const res = await Client.delete(`/api/stocks/${id}`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
 }
