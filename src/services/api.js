c4d226d98247c2a5acb3a5535973f6b457fd0f87
Jonshea Nutson <yuurieru@icloud.com>
2023-03-07 10:15:12 -0500

    changed proxy for apii calls

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,18 @@
-import axios from "axios"
+import axios from 'axios'
 
-export const BASE_URL = "http://localhost:3001"
+export const BASE_URL = 'https://stockwise-api.herokuapp.com/'
 
 const Client = axios.create({ baseURL: BASE_URL })
 
 Client.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token")
-    if (token) {
-      config.headers["authorization"] = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => Promise.reject(error)
+    (config) => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers['authorization'] = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => Promise.reject(error)
 )
 
 export default Client
