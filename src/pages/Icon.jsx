516d19bf8490b6a1c47e1a1a7a2477e76aad5c21
mannyaalonso <mannyaalonso@gmail.com>
2023-03-07 09:41:35 -0500

    static images and favicon

---
+++ src/pages/Icon.jsx
@@ -0,0 +1,11 @@
+import { AiOutlineStock } from "react-icons/ai"
+
+const Icon = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <AiOutlineStock className="mx-auto h-5/6 w-auto bg-gradient-to-r from-[#4abea3] to-[#64fcd9] text-white rounded-full p-2" />
+    </div>
+  )
+}
+
+export default Icon
